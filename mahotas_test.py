626730be091a176b7b00980c3224b65e1439477c
hendrik <hendrikheuer@gmail.com>
2013-12-16 10:39:27 +0100

    can plot stuff

--- mahotas_test.py
+++ mahotas_test.py
@@ -1,10 +1,15 @@
 #!/opt/local/bin/python3.3
 
+import sys
 import numpy as np
 import mahotas as mh
-import pylab as P
 
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
+print
 print("*** Mahotas Test ***")
+print
 
 filename = "/Users/hendrikheuer/Downloads/mac_wallpaper.jpg"
 
@@ -16,17 +21,19 @@ def read_and_generate_histogram():
 
 def draw_histogram():
 	mu, sigma = 200, 25
-	x = mu + sigma*P.randn(10000)
+	x = mu + sigma*np.random.randn(10000)
 
 	# the histogram of the data with histtype='step'
-	n, bins, patches = P.hist(x, 50, normed=1, histtype='stepfilled')
-	P.setp(patches, 'facecolor', 'g', 'alpha', 0.75)
+	n, bins, patches = plt.hist(x, 50, normed=1, histtype='stepfilled')
+	plt.setp(patches, 'facecolor', 'g', 'alpha', 0.75)
 
 	# add a line showing the expected distribution
-	y = P.normpdf( bins, mu, sigma)
-	l = P.plot(bins, y, 'k--', linewidth=1.5)
+	y = mlab.normpdf( bins, mu, sigma)
+	l = plt.plot(bins, y, 'k--', linewidth=1.5)
 
-	P.figure()
+	#plt.plot(range(10), range(10))
+	plt.title("Simple Plot")
+	plt.show()
 
 def main(filename):
 	#read_and_generate_histogram( filename )
