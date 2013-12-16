d8c6ac0166d035b43f85c7c00416132bf7aff68e
hendrik <hendrikheuer@gmail.com>
2013-12-16 11:17:21 +0100

    added read each file in folder

--- plot_folder_as_histogram.py
+++ plot_folder_as_histogram.py
@@ -1,6 +1,9 @@
 #!/opt/local/bin/python3.3
 
+import os
 import sys
+import re
+
 import numpy as np
 import mahotas as mh
 
@@ -8,11 +11,19 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
 print
-print("*** Mahotas Test ***")
+print("plot_folder_as_histogram foldername")
 print
 
-def read_and_generate_histogram( filename ):
-	img = mh.imread( filename )
+def read_each_file_in_folder( path ):
+	print("Reading folder: " + path)
+	
+	dirs = os.listdir( path )
+	for a_file in dirs:
+		if a_file.endswith(".jpg"):
+			print(a_file)
+
+def read_and_generate_histogram( folder ):
+	img = mh.imread( folder )
 	hist, bin_edges = np.histogram( img, bins=12)
 
 	return hist, bin_edges
@@ -32,10 +43,11 @@ def draw_histogram( histogram ):
 	plt.title("Histogram")
 	plt.show()
 
-def main(filename):
-	hist, bin_edges = read_and_generate_histogram( filename )
+def main(folder):
+	#hist, bin_edges = read_and_generate_histogram( folder )
+	#draw_histogram( hist )
 
-	draw_histogram( hist )
+	read_each_file_in_folder(folder)
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+		main(sys.argv[1])
