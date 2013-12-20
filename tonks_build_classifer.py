749618cd12c443c0cc55d560bc7fbc44dc42de03
hendrik <hendrikheuer@gmail.com>
2013-12-20 10:53:37 +0100

    knn works!!!

--- tonks_build_classifer.py
+++ tonks_build_classifer.py
@@ -11,8 +11,8 @@ import pickle
 import code
 
 import pylab as pl
-from sklearn import linear_model, datasets
-from sklearn.preprocessing import Imputer
+
+from sklearn import neighbors
 
 HIST_BANDS = 128
 ALPHA_BANDS = 0.1
@@ -22,19 +22,16 @@ print("tonks: Build classifier: STARTED")
 
 #### CLASSES ####
 
-class ImageClassifier():
+class Histogram():
 
-	def transform( self, image ):
-		img = mh.imread( image )
+	def get_histogram_from_image( image_path ):
+		img = mh.imread( image_path )
 
-		hist, bin_edges = np.histogram( image, bins = range(HIST_BANDS), normed=True)
+		hist, bin_edges = np.histogram( img, bins = range(HIST_BANDS), normed=True)
 		hist = hist.clip(0.0,0.1)
 
 		return hist
 
-	def predict(self, hist ):
-		pred = self.clf.predict( hist )
-		return {"scores" : pred}
 
 #### HELPER FUNCTIONS ####
 
@@ -76,78 +73,25 @@ def main():
 				
 			color_class_id = color_class_id + 1
 
-		iris = datasets.load_iris()
-		Xiris = iris.data  # we only take the first two features.
-		Yiris = iris.target
+		X = np.asarray( X , dtype=np.float64 )
+		Y = np.asarray( Y , dtype=np.float64 )
 
-		X = np.array( np.asarray( X , dtype=np.float64 ) )
-		Y = np.ravel( np.asarray( Y , dtype=np.float64 ) )
+		Y = np.ravel( Y )
 
-		"""
-		imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+		knn = neighbors.KNeighborsClassifier(n_neighbors=2)
+		print(knn)
 
-		X = imp.fit( X )
-		X = imp.transform( X )
+		knn.fit( X, Y )
+
+		h = Histogram.get_histogram_from_image("/Users/hendrikheuer/Desktop/mac_wallpaper.jpg")
 		
-		Y = imp.fit( Y )
-		Y = imp.transform( Y )
-		"""
+		print( len(h) )
 
-		print( X )
-		print( Y )
-
-		print( X.shape )
-		print( Y.shape )
-
-		print( "dataset.dtype")
-		print( X.dtype )
-		print( Y.dtype )
-
-		print( "isfinite")
-		print( np.all(np.isfinite( X )) )
-		print( np.all(np.isfinite( Y )) )
-
-		"""
-		print( "X" )
-		print( X )
-		print( "Y" )
-		print(  Y )
-
-		return
-		"""
-
-		h = .02  # step size in the mesh
-
-		logreg = linear_model.LogisticRegression(C=1e5)
-
-		#code.interact(local=locals())
-
-		# we create an instance of Neighbours Classifier and fit the data.
-		logreg.fit(X, Y)
-
-		# Plot the decision boundary. For that, we will assign a color to each
-		# point in the mesh [x_min, m_max]x[y_min, y_max].
-		x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-		y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-		xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+		colour_match = int( knn.predict( h )[0] )
 		
-		Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
-		# Put the result into a color plot
-		Z = Z.reshape(xx.shape)
-		pl.figure(1, figsize=(4, 3))
-		pl.pcolormesh(xx, yy, Z, cmap=pl.cm.Paired)
-
-		# Plot also the training points
-		pl.scatter(X[:, 0], X[:, 1], c=Y, edgecolors='k', cmap=pl.cm.Paired)
-		pl.xlabel('Sepal length')
-		pl.ylabel('Sepal width')
-
-		pl.xlim(xx.min(), xx.max())
-		pl.ylim(yy.min(), yy.max())
-		pl.xticks(())
-		pl.yticks(())
-
-		pl.show()
+		print( colour_match )
+		print( color_names[colour_match] )
+	
 
 	else:
 		print("tonks: ERROR: couldn't find histograms - Regenerate?")
